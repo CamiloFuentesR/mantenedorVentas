9a0b12b169c4c0476d6474ba318d50808b4bee4f
CamiloFuentesR <lokomilo1@gmail.com>
2021-04-30 03:38:12 -0400

    diseño

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -10,50 +10,43 @@ export const LoginScreen = () => {
 
     return (
         <div>
-            <NavBar/>
-            <div>
-            <div className="limiter">
-		<div className="container-login100">
-			<div className="wrap-login100">
-				<div className="login100-form-title" style={{backgroundImage: login}} >
-					<span className="login100-form-title-1" >
-						Inicio de Sesión
-					</span>
-				</div>
-
-				<form className="login100-form validate-form">
-					<div className="wrap-input100 validate-input m-b-26" data-validate="Username is required">
-						<span className="label-input100">Username</span>
-						<input className="input100" type="text" name="username" placeholder="Enter username"/>
-						<span className="focus-input100"></span>
-					</div>
-
-					<div className="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
-						<span className="label-input100">Password</span>
-						<input className="input100" type="password" name="pass"autoComplete="false" placeholder="Enter password"/>
-						<span className="focus-input100"></span>
-					</div>
-
-					<div className="flex-sb-m w-full p-b-30">
-						<div className="contact100-form-checkbox">
-							
-						</div>
-					</div>
-
-					<div className="container-login100-form-btn">
-						<button 
-                            type="submit"
-                            className="login100-form-btn "
-                            onClick={handleSubmit}
-                            >
-                            
-							Login
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+            <NavBar />
+            <div className="animate__animated animate__fadeIn animate__faster">
+                <div className="limiter">
+                    <div className="container-login100">
+                        <div className="wrap-login100">
+                            <div className="login100-form-title" style={{ backgroundImage: login }} >
+                                <span className="login100-form-title-1" >
+                                    Inicio de Sesión
+					            </span>
+                            </div>
+                            <form className="login100-form validate-form">
+                                <div className="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+                                    <span className="label-input100">Username</span>
+                                    <input className="input100" type="text" name="username" placeholder="Enter username" />
+                                    <span className="focus-input100"></span>
+                                </div>
+                                <div className="wrap-input100 validate-input m-b-18" data-validate="Password is required">
+                                    <span className="label-input100">Password</span>
+                                    <input className="input100" type="password" name="pass" autoComplete="false" placeholder="Enter password" />
+                                    <span className="focus-input100"></span>
+                                </div>
+                                <div className="flex-sb-m w-full p-b-30">
+                                    <div className="contact100-form-checkbox">
+                                    </div>
+                                </div>
+                                <div className="container-login100-form-btn">
+                                    <button
+                                        type="submit"
+                                        className="login100-form-btn "
+                                        onClick={handleSubmit}
+                                    >Login
+						            </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
